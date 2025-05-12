2d2e54a7bcc84ca41be93516b95989c66fd5b785
Andrea Loreti <andrea.loreti@stfc.ac.uk>
2025-05-12 16:54:00 +0100

    NaN excluded from average

--- scripts/benchmarking/pca_utils.py
+++ scripts/benchmarking/pca_utils.py
@@ -1,14 +1,11 @@
+"""
+Utilities for processing MAST signals.
 
-import cv2
-import fsspec
+How to use it: python pca_utils.py
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import colors
 import os
-import pandas as pd
-import s3fs
-import xarray as xr
-import zarr
 import json
 
 import sys
@@ -19,10 +16,17 @@ sys.path.append(mast_tools_path)
 from signal_utils import MASTSignalManager  
 from store_utils import MASTStorageManager
 
+def is_finite_numeric_array(arr):
+    # Check if array is numeric
+    if not np.issubdtype(arr.dtype, np.number):
+        return False
+    # Check if all values are finite (no NaN or Inf)
+    return np.isfinite(arr).all()
+
 def read_signals(filepath: str)->dict[str:int]:
     """
     filepath: path to file containing all 
-    signal names and their multeplicity (nr. of traces)
+    signal names and their multeplicity (nr. of channels)
 
     Output: Dictionary with signal names as keys and 
     number of traces as values
@@ -40,18 +44,38 @@ def read_signals(filepath: str)->dict[str:int]:
         print(f"An error occurred while reading the file '{filepath}': {e}")
         return {}
     return signals
+    
 
 def signals_average_across_shots(store_manager : MASTStorageManager, 
+                                 shot_ids : list[int],
                                  filepath:str,
                                  local : bool,
                                  singularity: bool):
     """
-    Summary: calucalte average values of MAST signals across all shots available. 
-    Signals are listed inside the file found at filepath.
-    """
+    For each shot:
 
-    # Get all shot ids
-    shot_ids = store_manager.list_all_shots(local=local, singularity = singularity)
+    1- considers all signal vectors listed in the 
+    data/list_of_signals.txt 
+
+    2- calculates the average value of each channel 
+    in the signal. 
+
+    3- Set to zero channels that contain NaN;
+
+    3- returns the average value across shots in a json format:
+    {
+        "data": [
+            {
+                "signal_name": [ 
+                    [avergae channel 0 ],
+                    [average channel 1],
+                    ...
+                ],
+                ...
+            }
+        ]
+    }
+    """
 
     # Read file containing signal names and the number of channels for each signal
     data_set = read_signals(filepath)
@@ -60,49 +84,59 @@ def signals_average_across_shots(store_manager : MASTStorageManager,
     data_set_sums = {}
 
     # Loop through shots
-    for shot_id in shot_ids[:2]:
+    for nr,shot_id in enumerate(shot_ids[:5]):
+
+        if nr>=0 :
+            print(f"Shot number {nr+1} out of {len(shot_ids)}")
         store = store_manager.make_shot_store(shot_id, local=local, singularity = singularity)
 
         for key, value in data_set.items():
-            
             # Retrieve signal group and name
-            sub_keys = key.split("/")
-            group = sub_keys[0]
-            signal_name = sub_keys[1]
+            group, signal_name = key.split("/")
 
             # Form instance of SIGNAL
             sig = MASTSignalManager(group, signal_name, shot_id)
 
-            # Initialize datasets that will run through shots
-            if value not in data_set_counter:
-                data_set_counter[signal_name] = 0
-            if value not in data_set_sums:
-                data_set_sums[signal_name] = None
+            # Prepare tracking
+            data_set_counter.setdefault(signal_name, np.zeros(value))
+            data_set_sums.setdefault(signal_name, None)
 
             try:
                 vals = sig.get_values(store)
-                data_set_counter[signal_name] += 1
 
+                mean_vals = np.mean(vals, axis=1) # Returns NaN if NaN is in vals
+
+                # Check mean_vals are all numeric different from NaN and Inf
+                for i, mean_val in enumerate(mean_vals):
+
+                    if is_finite_numeric_array(mean_val):
+                        data_set_counter[signal_name][i] += 1
+                    else:
+                        mean_vals[i]=0
+
+                # Add mean_vals to data_set_sums
                 if data_set_sums[signal_name] is None:
-                    # Initialize the summed array with the first array's shape and data type
-                    data_set_sums[signal_name] =  np.nan_to_num(vals)
+                    data_set_sums[signal_name] =  mean_vals
                 else:
-                    data_set_sums[signal_name] += np.nan_to_num(vals)
+                    data_set_sums[signal_name] += mean_vals
 
             except Exception as e:
                 print(f"Exception: {e}")
 
     # Calculate average 
-    averaged_arrays = {}
     data = []
-    for value, summed_array in data_set_sums.items():
+    for signal_name, summed_array in data_set_sums.items():
+        
+        counter = data_set_counter[signal_name]
+        
+        averaged_channels = []
+        for channel, count in zip(summed_array, counter):
+            if channel and count> 0:
+                averaged_channels.append(channel / count)
+            else:
+                print(f"No valid data to average for value '{signal_name}'.")
 
-        counter = data_set_counter[value]
-        if summed_array is not None and counter > 0:
-            averaged_arrays[value] = summed_array / counter
-            data.append({value: averaged_arrays[value].tolist()})
-        else:
-            print(f"No valid data to average for value '{value}'.")
+        data.append({signal_name: averaged_channels})
     
     json_data = {"data":data}
     with open("averaged_arrays.json", "w") as f:
@@ -111,6 +145,13 @@ def signals_average_across_shots(store_manager : MASTStorageManager,
 
 
 if __name__ == "__main__":
-    breakpoint()
+     # Get all shot ids
+    local = False
+    singularity = True
     store_manager = MASTStorageManager(local_root_path = "/srv")
-    signals_average_across_shots(store_manager,"data/list_of_signals.txt", False, True)
+    shot_ids = store_manager.list_all_shots(local= local, singularity = singularity)
+    signals_average_across_shots(store_manager,
+                                shot_ids[:5], 
+                                "data/list_of_signals.txt", 
+                                local,
+                                singularity)
