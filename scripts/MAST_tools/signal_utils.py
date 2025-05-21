4c6636d5791ffff0b933d2126a3506596effc34a
RODRIGO ORDONEZ-HURTADO <rodrigo.ordonez.hurtado@ibm.com>
2025-05-21 16:25:34 +0100

    Minor changes in signal_utils.

--- scripts/MAST_tools/signal_utils.py
+++ scripts/MAST_tools/signal_utils.py
@@ -32,7 +32,8 @@ class MASTSignalManager:
         manager_id : str
             User defined manager ID. Default: "".
         store_manager_settings : Union[dict, None]
-            Settings for the store manager instance.
+            Settings for the store manager instance. If None, a generic store manage instance is created with default
+            values as defined in the docstrings of store_utils.MASTStorageManager.
             Optional. Default: None.
 
         Methods
@@ -41,12 +42,30 @@ class MASTSignalManager:
             Get source profiles from a given data origin.
         get_signal_values(data_origin, source_name, signal_name)
             Get signal values from a given data origin.
+        _set_store_manager(store_manager)
+            Not yet implemented.
+
+        Remarks
+        -------
+        - Upon creation of a signal manager instance from the MASTSignalManager class, a store manager (from the
+          MASTStorageManager class is created as instance attribute. This facilitates the process to get signal values,
+          as no separate store manager instance must be created. However, if an existing store manager instance is
+          available, it could be passed
 
         """
 
         self.signal_manager_id = manager_id
-        self.store_manager_settings = {} if store_manager_settings is None else store_manager_settings
+
+        if store_manager_settings is None:
+            self.store_manager_settings = {}
+        else:
+            assert isinstance(store_manager_settings)
+            self.store_manager_settings = store_manager_settings
         self.store_manager = store_utils.MASTStorageManager(**self.store_manager_settings)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _set_store_manager(self, store_manager):
+        raise NotImplementedError
 
     # ------------------------------------------------------------------------------------------------------------------
     def get_source_profiles(
@@ -85,16 +104,16 @@ class MASTSignalManager:
     # ------------------------------------------------------------------------------------------------------------------
     def get_signal_values(
             self,
+            signal_name: str,
             data_origin: Union[dict, cc.ZarrStoreType, cc.XarrayDatasetType],
-            source_name: Union[str, cc.NoneType] = None,
-            signal_name: Union[str, cc.NoneType] = None
+            source_name: Union[str, cc.NoneType] = None
     ):
         """
         Get signal values from a given data origin.
 
         Parameters
         ----------
-        data_origin : Union[dict, ZarrStoreType]
+        data_origin : Union[dict, ZarrStoreType, cc.XarrayDatasetType]
             Origin of data for signal value retrieval.
         source_name : str
             Name of target source.
@@ -109,6 +128,8 @@ class MASTSignalManager:
             Signal values.
 
         """
+        assert isinstance(signal_name, str), "Type error: invalid source_name. It must be of type str."
+
         if isinstance(data_origin, cc.XarrayDatasetType):
             # From group profile (i.e., xarray.core.dataset)
             profile = data_origin
@@ -116,7 +137,6 @@ class MASTSignalManager:
         else:
             self.store_manager._check_data_origin(data_origin)  # noqa.
             assert isinstance(source_name, str), "Type error: invalid source_name. It must be of type str."
-            assert isinstance(signal_name, str), "Type error: invalid source_name. It must be of type str."
 
             if isinstance(data_origin, dict):
                 # From shot info
@@ -147,9 +167,9 @@ def test():
 
     store_from_shot_info = signal_manager.store_manager.make_shot_store(shot_info=shot_info)
     signal_values = signal_manager.get_signal_values(
+        signal_name=signal_name,
         data_origin=store_from_shot_info,
-        source_name=source_name,
-        signal_name=signal_name
+        source_name=source_name
     )
 
     print(f"Signal values: {signal_values}\n")
@@ -158,9 +178,9 @@ def test():
     # Get signal values from shot ID
 
     signal_values = signal_manager.get_signal_values(
+        signal_name=signal_name,
         data_origin=shot_info,
-        source_name=source_name,
-        signal_name=signal_name
+        source_name=source_name
     )
 
     print(f"Signal values: {signal_values}\n")
