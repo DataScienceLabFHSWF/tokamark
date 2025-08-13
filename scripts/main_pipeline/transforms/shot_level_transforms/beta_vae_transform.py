7066bb46f6bbcb77133ce07fe8def98c71d97c8b
George Holt <george.holt@stfc.ac.uk>
2025-08-13 10:34:18 +0100

    Use defaultdict to initialize dict storing all signals in BetaVAETransform

--- scripts/main_pipeline/transforms/shot_level_transforms/beta_vae_transform.py
+++ scripts/main_pipeline/transforms/shot_level_transforms/beta_vae_transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 
 class BetaVAETransform:
@@ -24,18 +25,14 @@ class BetaVAETransform:
 
         for sample in list_samples:
             # Extract all signals from both x and y windows
-            all_signals = {}
+            all_signals = defaultdict(list)
 
             # Add x signals
             for signal_name, signal_data in sample["x"].items():
-                if signal_name not in all_signals:
-                    all_signals[signal_name] = []
                 all_signals[signal_name].append(signal_data["values"])
 
             # Add y signals
             for signal_name, signal_data in sample["y"].items():
-                if signal_name not in all_signals:
-                    all_signals[signal_name] = []
                 all_signals[signal_name].append(signal_data["values"])
 
             # Create samples for each unique signal
