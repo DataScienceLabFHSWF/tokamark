c5fed2df9e072e6d6f3808a137a1627a7b394378
Cecile Rousseau <rousseau.cecile@ibm.com>
2025-09-22 13:19:33 +0100

    removing psi transpose for remote data from MAST dataset

--- scripts/MAST_tools/MAST_dataset.py
+++ scripts/MAST_tools/MAST_dataset.py
@@ -80,7 +80,7 @@ class MastDataset(Dataset):
                     print(f"Error getting time for shot {self.shots_list[idx]}: {e}")
                     shot_time = None
                 try:
-                    if (f"{source}-{signal}" in ["equilibrium-psi", "equilibrium-lcfs_z", "equilibrium-lcfs_r"]) and self.local == False:
+                    if (f"{source}-{signal}" in ["equilibrium-lcfs_z", "equilibrium-lcfs_r"]) and self.local == False:
                         print(f"Transposing values bc {source}-{signal} not saved the same in remote location")
                         shot_vals = np.moveaxis(shot_profile.values, 0, -1)
                     else:
