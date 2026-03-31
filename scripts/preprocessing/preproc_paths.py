343ee20e2ba039be3eee5cbe87d4d60f834df4ad
RODRIGO ORDONEZ-HURTADO <Rodrigo.Ordonez.Hurtado@ibm.com>
2026-03-31 10:59:20 +0100

    Fix to final pending issues, deletion of unnecessary files.

--- scripts/preprocessing/preproc_paths.py
+++ scripts/preprocessing/preproc_paths.py
@@ -13,10 +13,9 @@ SHOT_STATS_DIR = ARTIFACTS_DIR / "shots_stats"
 
 OUTPUT_DIR = PROJECT_ROOT_DIR / "output"
 
-# TODO [Cecile/Rodrigo]: Check with Cecile impact of new filenames.
-DEFAULT_SHOTS_STATS_TEST_FILE = SHOT_STATS_DIR / "shots_stats_test.csv"  # Before: shot_statistics_test_NEW.csv
-DEFAULT_SHOTS_STATS_TRAIN_FILE = SHOT_STATS_DIR / "shots_stats_train.csv"  # Before: shot_statistics_train_NEW.csv
-DEFAULT_SHOTS_STATS_VAL_FILE = SHOT_STATS_DIR / "shots_stats_val.csv"  # Before: shot_statistics_val_NEW.csv
+DEFAULT_SHOTS_STATS_TEST_FILE = SHOT_STATS_DIR / "shots_stats_test.csv"
+DEFAULT_SHOTS_STATS_TRAIN_FILE = SHOT_STATS_DIR / "shots_stats_train.csv"
+DEFAULT_SHOTS_STATS_VAL_FILE = SHOT_STATS_DIR / "shots_stats_val.csv"
 
 SIGNALS_STATS_DIR = ARTIFACTS_DIR / "signals_stats"
 DEFAULT_SIGNALS_MEAN_STD_TRAIN_FILE = SIGNALS_STATS_DIR / "dict_signals_mean_std_train.yaml"
