24376053762049069b906b744b4080befd3e429b
Cecile Rousseau <rousseau.cecile@ibm.com>
2026-04-13 11:03:47 +0100

    Add DEFAULT_SHOTS_STATS_ALL_FILE

--- scripts/preprocessing/preproc_paths.py
+++ scripts/preprocessing/preproc_paths.py
@@ -14,6 +14,7 @@ PREPROC_STATS_DIR = ARTIFACTS_DIR / "preprocessing_stats"
 
 OUTPUT_DIR = PROJECT_ROOT_DIR / "output"
 
+DEFAULT_SHOTS_STATS_ALL_FILE = SHOT_STATS_DIR / "shots_stats_all.csv"
 DEFAULT_SHOTS_STATS_TEST_FILE = SHOT_STATS_DIR / "shots_stats_test.csv"
 DEFAULT_SHOTS_STATS_TRAIN_FILE = SHOT_STATS_DIR / "shots_stats_train.csv"
 DEFAULT_SHOTS_STATS_VAL_FILE = SHOT_STATS_DIR / "shots_stats_val.csv"
